21ff2a89f7cdd0f53b70a85dba2fbc2e33fc58b0
Xakse Xakse <roma.roma.romanov2001@gmail.com>
2026-06-13 10:57:26 +0300

    feat: add react router and page placegolder

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-50">
+      <div className="p-8 bg-white rounded-lg shadow-md w-96">
+        <p>КОНТЕНТ</p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import LoginPage from "./pages/login/login";
+import RegistrationPage from "./pages/registration/registration";
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/registration",
+    element: <RegistrationPage />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/login" replace />,
+  },
+]);
